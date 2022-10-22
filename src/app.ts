e4b8305e4bf4fd13fa404d9509cae612e66430f8
zsuzsasovari <105643090+zsuzsasovari@users.noreply.github.com>
2022-10-22 14:38:18 +0200

    kész

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 // Importáld be a HumanHero és TransformerHero osztályokat.
-
+import {HumanHero} from './hero' ;
+import {TransformerHero} from './transformer';
 
 /**
  * Állítsd be helyesen a változót!
@@ -8,7 +9,11 @@
  * Hozz létre benne 3 HumanHero példányt, szabadon választott adatokkal.
  * @var {HumanHero[]} humans
  */
-export const humans: any[] = [];
+export const humans: any[] = [
+   new HumanHero (1, 'Hero1', 'male', 100, 200),
+   new HumanHero(2, 'Hero2', 'male', 300, 400),
+   new HumanHero(3, 'Hero3', 'male', 500, 600)
+];
 
 /**
  * Állítsd be helyesen a változót!
@@ -17,4 +22,8 @@ export const humans: any[] = [];
  * Hozz létre benne 3 TransformerHero példányt, szabadon választott adatokkal.
  * @var {TransformerHero[]} transformers
  */
-export const transformers: any[] = [];
+export const transformers: any[] = [
+    new TransformerHero (1, 'Transformer1', 1, 2, 'robot'),
+    new TransformerHero(2, 'Transformer2', 4, 5, 'robot'),
+    new TransformerHero(3, 'Transformer3', 7, 8, 'robot')
+];
